322fd41a26ba80ecb71278e41dcd93e9470d21a2
Vladurka <0986535843v@gmail.com>
2025-09-20 19:18:02 +0200

    Setup.py ready

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from typing import List
+
+def get_requirements() -> List[str]:
+    requirement_list: List[str] = []
+    try:
+      with open("requirements.txt", 'r') as file:
+          lines = file.readlines()
+          for line in lines:
+              requirement = line.strip()
+              if requirement and requirement != "-e .":
+                  requirement_list.append(requirement)
+    
+    except FileNotFoundError:
+        print(f"Error: The file requirements.txt was not found.")
+
+    return requirement_list
+
+
+setup(
+    name="NetworkSecurity",
+    version="0.0.1",
+    author="My name",
+    author_email="v.vezerian@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
